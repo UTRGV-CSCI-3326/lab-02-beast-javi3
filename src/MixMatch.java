697f68733d719371faba308bf49f904a43e8c781
beast-javi3 <javiala0104@gmail.com>
2025-01-28 00:01:03 +0000

    Second commit

---
+++ src/MixMatch.java
@@ -0,0 +1,31 @@
+public class MixMatch{
+   public static void main(String[] args){
+    int value = 7;
+    System.out.println(value +"  " +"==  Integer will storage whole numbers.");
+
+    String first =  "hello";
+    System.out.println(first + " "+ "== String can store mutiple words");
+
+    float valuef = 5.6f;
+    System.out.println(valuef + " "+"== float is able to store whole and decimal values");
+
+    String value_char = "g";
+    System.out.println(value_char + "  ==  char is able to store up single letters.");
+
+    double value_double = 2.55555;
+    System.out.println(value_double + " double can store more decimal values then float. ");
+
+    short age = 20;
+    short hight = 6;
+    System.out.println(age + "  age");
+    System.out.println(hight + "  hight" + " ==  shor is a way the user can use to store two integers when the memory is almost full.");
+
+       long population = 84874562;
+    System.out.println(population + " ==  long is a way for the user to be able to storage large whole numbers. ");
+
+    boolean schoolisfun = true;
+    boolean schoolisdumb = false;
+    System.out.println(schoolisfun);
+    System.out.println(schoolisdumb + "==  boolean is only able to storage true or false values." );
+}
+}
